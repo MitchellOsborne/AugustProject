71755477052a5e70c19438e7d3c4a0b665d48579
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-08-14 13:47:35 +1000

    no message

--- EventHorizon/Assets/TestMove.cs
+++ EventHorizon/Assets/TestMove.cs
@@ -28,28 +28,29 @@ public class TestMove : MonoBehaviour {
 			notBraking = false;
 		}
 
-		if(Angle.x != 0 || Angle.y != 0)
-		{
-					Angle.x *= -1;
-					float tempAngle = Mathf.Atan2 (Angle.x, Angle.y) * Mathf.Rad2Deg;
-					Vector3 tempVec = new Vector3(0,0,tempAngle);
-					//Debug.Log (tf.rotation.eulerAngles);
-					Quaternion nyuk = Quaternion.Slerp (tf.rotation,Quaternion.Euler(tempVec), 5*Time.deltaTime);
-					Debug.Log (nyuk);
-					tf.rotation = (nyuk);
-		}
-		Debug.Log (tf.rotation.eulerAngles);
-		if (notBraking) {
-			rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime));
-		} else {
-			brakeEnergy += Time.deltaTime;
-		}
-		if (Input.GetAxis("Brake") == 0 && notBraking == false) {
-			rb.velocity = Vector3.zero;
-			rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime) * brakeEnergy);
-			notBraking = true;
-		}
+		if (Angle.x != 0 || Angle.y != 0) {
+			Angle.x *= -1;
+			float tempAngle = Mathf.Atan2 (Angle.x, Angle.y) * Mathf.Rad2Deg;
+			Vector3 tempVec = new Vector3 (0, 0, tempAngle);
+			//Debug.Log (tf.rotation.eulerAngles);
+			Quaternion nyuk = Quaternion.Slerp (tf.rotation, Quaternion.Euler (tempVec), 5 * Time.deltaTime);
+			Debug.Log (nyuk);
+			tf.rotation = (nyuk);
+		
+			Debug.Log (tf.rotation.eulerAngles);
+			if (notBraking) {
+				rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime));
+			} else {
+				brakeEnergy += 100 * Time.deltaTime;
+			}
+			if (Input.GetAxis ("Brake") == 0 && notBraking == false) {
+				//rb.velocity = Vector3.zero;
+				rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime) * brakeEnergy);
+				notBraking = true;
+				brakeEnergy = 0;
 
+			}
+		}
 
 		Debug.Log (rb.velocity);
 
