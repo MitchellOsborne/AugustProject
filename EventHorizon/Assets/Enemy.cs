0ea3e6ff2fc5b5d5b6ce1a132cb2f621ea221bed
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-08-14 18:45:29 +1000

    Added enemy UFO chasing the player

---
+++ EventHorizon/Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public float speed;
+	Rigidbody rb;
+	GameObject target;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		target = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tempVec = rb.position - target.transform.position;
+		tempVec.Normalize ();
+		rb.position -= tempVec *(speed * Time.deltaTime);
+	}
+}
