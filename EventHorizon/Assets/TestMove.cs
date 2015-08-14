4ce689e1fb788012eba2cd3203f4822d4f2adc75
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-08-14 14:38:23 +1000

    no message

--- EventHorizon/Assets/TestMove.cs
+++ EventHorizon/Assets/TestMove.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class TestMove : MonoBehaviour {
 	public float ShipSpeed = 100;
 	public float MaxVelocity;
-	public float RotateSpeed = 10.0f;
-	float brakeEnergy = 0;
-	bool notBraking = true;
+	public float RotateSpeed = 20.0f;
+	public float maxBoosterEnergy = 400;
+	public ParticleSystem boosterPS;
+	public ParticleSystem thrusterPS;
+	float boosterEnergy = 0;
+	bool notBoosting = true;
 	Vector3 Angle;
 	Rigidbody rb;
 	Transform tf;
@@ -25,34 +28,58 @@ public class TestMove : MonoBehaviour {
 		//Debug.Log (Angle);
 		Debug.Log (Quaternion.Euler(Angle));
 		if (Input.GetAxis ("Brake") != 0) {
-			notBraking = false;
+			notBoosting = false;
 		}
 
-		if (Angle.x != 0 || Angle.y != 0) {
-			Angle.x *= -1;
-			float tempAngle = Mathf.Atan2 (Angle.x, Angle.y) * Mathf.Rad2Deg;
-			Vector3 tempVec = new Vector3 (0, 0, tempAngle);
-			//Debug.Log (tf.rotation.eulerAngles);
-			Quaternion nyuk = Quaternion.Slerp (tf.rotation, Quaternion.Euler (tempVec), 5 * Time.deltaTime);
-			Debug.Log (nyuk);
-			tf.rotation = (nyuk);
-		
-			Debug.Log (tf.rotation.eulerAngles);
-			if (notBraking) {
+		if(Angle.x != 0 || Angle.y != 0)
+		{
+					Angle.x *= -1;
+					float tempAngle = Mathf.Atan2 (Angle.x, Angle.y) * Mathf.Rad2Deg;
+					Vector3 tempVec = new Vector3(0,0,tempAngle);
+					//Debug.Log (tf.rotation.eulerAngles);
+			float rot = RotateSpeed - rb.velocity.magnitude;
+			if(!notBoosting)
+			{
+				rot = RotateSpeed = 0;
+			}
+			if(rot < 0)
+			{
+				rot *= -1;
+			}
+			Debug.Log(rot);
+			
+					Quaternion nyuk = Quaternion.Slerp (tf.rotation,Quaternion.Euler(tempVec), rot*Time.deltaTime);
+			
+					Debug.Log (nyuk);
+					tf.rotation = (nyuk);
+
+			if (notBoosting) {
 				rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime));
+				rb.drag = 0.2f;
 			} else {
-				brakeEnergy += 100 * Time.deltaTime;
-			}
-			if (Input.GetAxis ("Brake") == 0 && notBraking == false) {
-				//rb.velocity = Vector3.zero;
-				rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime) * brakeEnergy);
-				notBraking = true;
-				brakeEnergy = 0;
-
+				if(rb.velocity.magnitude < 10)
+				{
+				rb.drag = 0;
+				}else{
+					rb.drag = 0.2f;
+				}
+				thrusterPS.Stop();
+				boosterEnergy += 100*Time.deltaTime;
 			}
 		}
+		Debug.Log (tf.rotation.eulerAngles);
 
-		Debug.Log (rb.velocity);
+		if (Input.GetAxis("Brake") == 0 && notBoosting == false) {
+			//rb.velocity = Vector3.zero;
+			rb.AddForce (tf.up * (ShipSpeed * Time.deltaTime) * boosterEnergy);
+			notBoosting = true;
+			boosterEnergy = 0;
+			boosterPS.Emit(1000);
+			thrusterPS.Play();
+		}
+
+
+		Debug.Log (boosterEnergy);
 
 	}
 
