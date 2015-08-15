f70156cb612e3909ee25226617e4e47fa22268f6
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-08-15 15:58:19 +1000

    Bullets are now Agents, and can have behaviours added to them. The old projectile class has now been drastically cut down to accommodate its features being provided by Agent

---
+++ EventHorizon/Assets/DamageOnContact.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnContact : MonoBehaviour {
+	public float Damage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if(Other.tag == "Agent")
+		{
+			if(Other.GetComponent<Agent>().Team != gameObject.GetComponent<Agent>().Team)
+			{
+				Other.GetComponent<Agent>().Health -= Damage;
+				if(gameObject.GetComponent<Projectile>() != null)
+				{
+					Destroy(gameObject);
+				}
+			}
+		}
+	}
+}
